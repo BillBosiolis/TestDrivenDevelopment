41648fd27f30e290b7a88cc2d7d9ce465daaaa04
Bill Bosiolis <bbosiolis@gmail.com>
2023-06-13 19:21:06 +0300

    Chapter 8 - Making Objects.

--- src/main/java/com/oceanfeeling/tdd/Money.java
+++ src/main/java/com/oceanfeeling/tdd/Money.java
@@ -1,11 +1,21 @@
 package com.oceanfeeling.tdd;
 
-public class Money {
+public abstract class Money {
     protected int amount;
 
     @Override
     public boolean equals(Object o) {
         Money otherMoney = (Money) o;
         return amount == otherMoney.amount && getClass().equals(otherMoney.getClass());
+    }
+
+    public abstract Money times(int amount);
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
     }
 }
